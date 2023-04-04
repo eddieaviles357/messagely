b33b77da3dc79d180be72c497bb79106a5ede474
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-04-03 19:01:46 -0700

    added some data for users table and messages table

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     username text PRIMARY KEY,
@@ -13,9 +13,20 @@ CREATE TABLE users (
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
-    from_username text NOT NULL REFERENCES users,
-    to_username text NOT NULL REFERENCES users,
+    from_username text NOT NULL REFERENCES users ON DELETE CASCADE,
+    to_username text NOT NULL REFERENCES users ON DELETE CASCADE,
     body text NOT NULL,
     sent_at timestamp with time zone NOT NULL,
     read_at timestamp with time zone
 );
+
+INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) 
+VALUES
+('eddie123', '$2b$12$DxGUSeUO8T71X4waAGJvRODEXhqmXQmfk2wYAAdJeaSqTMZoizbYO', 'eddie', 'aviles', '9494444444',  NOW()::timestamp, NOW()),
+('nancy123','$2b$12$A89.yxoTFm/.iA3P9lJB1.wT1ejtzfihguyphNqIdSzpBzURq.z8O', 'nancy', 'aviles', '9494444444',  NOW()::timestamp, NOW()),
+('eric123','$2b$12$HenQaqTWBtrO0bI7mffCU.ZyIrjVtBc8hY8nr5.mfVen80FtqzGK.', 'eric', 'aviles', '9494444444',  NOW()::timestamp, NOW());
+
+INSERT INTO messages (from_username, to_username, body, sent_at, read_at)
+VALUES
+('eddie123', 'nancy123', 'hello nancy', NOW(), NOW()),
+('nancy123', 'eddie123', 'hello eddie', NOW(), NOW());
