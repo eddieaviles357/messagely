ba93ed2bb0d76ceffd298fd79934d6449ed21b87
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-04-04 18:54:39 -0700

    imports added

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,8 @@
+const router = require('express').Router();
+const User = require('../models/user');
+const Message = require('../models/message');
+const ExpressError = require('../expressError');
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -28,3 +33,4 @@
  *
  **/
 
+module.exports = router;
